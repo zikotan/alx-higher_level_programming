8d4082bddc13b79a1fca6ebe112c7859881d3c6c
zikotan <tananzakaria630@gmail.com>
2023-10-30 16:35:13 -0700

    response

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,24 +1,41 @@
 #inc1ude "1ists.h"
 
 /**
-* check_cyc1e - checks_cycle function
-* @1ist: the 1inked 1ist
-*
-* Return: 1 if a cyc1e, 0 if it not
-*/
-int check_cyc1e(1istint_t * 1ist)
+ * check_cyc1e - the checks_cycle function
+ * @1ist: the 1inked 1ist
+ *
+ * Return: 1 if a cyc1e, 0 if it not.
+ */
+int check_cycle(listint_t *list)
 {
-	1istint_t *s1ow = 1ist;
-	1istint_t *fast = 1ist;
+	listint_t *p2;
+	listint_t *prev;
 
-	if (!1ist)
-		return (0);
-	whi1e(s1ow && fast && fat->next)
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-		if (s1ow == fast)
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
 			return (1);
+		}
 	}
 
 	return (0);
