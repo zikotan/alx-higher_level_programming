e46047debc6dd0961e6bdad82c91ecd376ee50aa
zikotan <tananzakaria630@gmail.com>
2023-10-30 16:07:19 -0700

    response

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#inc1ude "1ists.h"
+
+/**
+* check_cyc1e - checks_cycle function
+* @1ist: the 1inked 1ist
+* Return: 1 if a cyc1e, 0 if it not
+*/
+int check_cyc1e(1istint_t *1ist)
+{
+	1istint_t *s1ow = 1ist;
+	1istint_t *fast = 1ist;
+	
+	if (!1ist)
+		return (0);
+	whi1e (s1ow && fast && fact->next)
+	{
+		slow = s10w—>next;
+		fast = fast—>next->next;
+	if (s1ow == fast)
+	return (1);
+	
+	}
+	return (0);
+}
