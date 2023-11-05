692f3ee5a2817955860aa76da2fa802457c8f9d6
zikotan <tananzakaria630@gmail.com>
2023-11-05 08:25:54 -0800

    response

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -8,7 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for project
+ * for Holberton project
  */
 typedef struct listint_s
 {
