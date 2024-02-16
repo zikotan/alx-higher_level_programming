9597fb49559ec04154578f42ffb30f87810ff511
zikotan <tananzakaria630@gmail.com>
2024-02-16 09:50:47 -0800

    response

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""taking in an argument and displays all values in the 
+"""taking in an argument and displays all values in the
 hbtn_0e_0_usa states where name matches the argument
 and is safe from SQL injections"""
-
 
 if __name__ == '__main__':
 
     import MySQLdb
     import sys
 
-    myDB = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    myDB = MySQLdb.connect(
+        host='localhost', port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
 
     myCursor = myDB.cursor()
     myCursor.execute("SELECT * FROM states WHERE name=%s\
